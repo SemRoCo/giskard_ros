09bd2dab4a738abd9d2814921d6f6f4d40ad6e55
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2016-02-17 15:30:03 +0100

    Added some scripts to test the pr2 control.

---
+++ scripts/cmd_pose1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from geometry_msgs.msg import PoseStamped
+
+def pose_publisher():
+    rospy.init_node('pose_publisher', anonymous=True)
+    pub = rospy.Publisher('pose_stamped_transformer/in', PoseStamped, queue_size=1)
+    rospy.sleep(.3)
+    if not rospy.is_shutdown():
+        msg = PoseStamped()
+        msg.header.frame_id = "base_link"
+        msg.pose.position.x = 0.377
+        msg.pose.position.y = 0.027
+        msg.pose.position.z = 1.054
+        msg.pose.orientation.x = -0.231
+        msg.pose.orientation.y = 0.792 
+        msg.pose.orientation.z = 0.561
+        msg.pose.orientation.w = 0.062
+        pub.publish(msg)
+
+if __name__ == '__main__':
+    try:
+        pose_publisher()
+    except rospy.ROSInterruptException:
+        pass
