5db2ae27c7ad0a0c91d98b46798954d67af87e5d
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2016-08-22 16:39:26 +0200

    More utils to read files from disc.

--- include/giskard_examples/ros_utils.hpp
+++ include/giskard_examples/ros_utils.hpp
@@ -22,8 +22,11 @@
 #ifndef __GISKARD_ROS_UTILS_HPP__
 #define __GISKARD_ROS_UTILS_HPP__
 
+#include <iostream>
+#include <fstream>
 #include <exception>
 #include <ros/ros.h>
+#include <ros/package.h>
 #include <geometry_msgs/Pose.h>
 #include <giskard_msgs/SemanticFloat64.h>
 
@@ -37,6 +40,37 @@ namespace giskard_examples
       throw std::runtime_error("Could not find parameter '" + name +
           "' in namespace '" + nh.getNamespace() + "'.");
     return param;
+  }
+
+  inline std::string read_file(const std::string& filename)
+  {
+    std::ifstream in(filename);
+    if(in.is_open())
+    {
+      std::string file_content, line;
+      while ( std::getline(in, line) )
+        file_content += line + "\n";
+
+      return file_content;
+    }
+    else
+      throw std::runtime_error("Could not open file '" + filename + "'.");
+  }
+
+  inline std::string read_ros_file(const std::string& filename, const std::string& package_name)
+  {
+    std::string package_path = ros::package::getPath(package_name);
+    return read_file(package_path + "/" + filename);
+  }
+
+  inline std::vector<std::string> read_ros_files(const std::vector<std::string>& filenames,
+      const std::string& package_name)
+  {
+    std::vector<std::string> result;
+    std::string package_path = ros::package::getPath(package_name);
+    for (size_t i=0; i<filenames.size(); ++i)
+      result.push_back(read_file(package_path + "/" + filenames[i]));
+    return result;
   }
 
   inline std::vector<giskard_msgs::SemanticFloat64> to_msg(
